f3e96d46fcceaa1631de33f5cd1f4837d7f2c6ac
szkqwer <3167142424@qq.com>
2020-03-14 11:40:54 +0800

    Login register

--- whu_oliverjudger_FE/src/pages/stu/views/ContestsInformation_Detail.vue
+++ whu_oliverjudger_FE/src/pages/stu/views/ContestsInformation_Detail.vue
@@ -1,28 +1,62 @@
 <template>
   <div class="contestsInformation_detail">
-    <span class="titleStyle" >{{title}}</span>
+    <span class="titleStyle" v-html=" getDetail (title)"></span>
 
-    <span class="detailContentStyle" v-html=" getDetail ()"></span>
+    <span class="detailContentStyle" v-html=" getDetail (detailContent)"></span>
+    <span class="contestRules">
+    <VariableTable
+      :table-data="tableData"
+      :column-headers="columnHeaders"
+      :show-header="true"
+    ></VariableTable>
+    </span>
   </div>
 </template>
 
 <script>
+import VariableTable from '../components/VariableTable'
 export default {
   name: 'ContestsInformation_Detail',
+  components: {
+    VariableTable
+  },
   data () {
     return {
-      title: '2019级《高级程序设计语言》过程性考核3-重现',
-      detailContent: '本次测试是本学期第3次过程性考核，主要考核以数组、指针、字符串等内容为主。\n\n' +
+      title: '',
+      detailContent: '',
+      columnHeaders: [ // 列表的头标签
+        {prop: 'startAt', label: '开始时间'},
+        {prop: 'endAt', label: '结束时间'},
+        {prop: 'contestType', label: '比赛类型'},
+        {prop: 'rule', label: '规则'},
+        {prop: 'creator', label: '发起者'}
+      ],
+      tableData: [] // 存储通告数据
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      this.title = '2019级《高级程序设计语言》过程性考核3-重现'
+      this.detailContent = '本次测试是本学期第3次过程性考核，主要考核以数组、指针、字符串等内容为主。\n\n' +
         '1.所有参加测试的学生需要事先在QDUOJ上注册账户，账户名统一为“班级－学号－姓名”的格式，例如“1班-2019000001-张三”。\n' +
         '\n' +
         '2.本次考核时间为两节课，采用计算机自动OI评测模式，评测结果按照解题总得分排名，排名最高者为100分，其他按照排名加权计算。\n' +
         '\n' +
         '3.考核密码为：19test3'
-    }
-  },
-  methods: {
-    getDetail () {
-      return this.detailContent.replace(/\n/g, '<br/>')
+      this.tableData = [
+        {
+          startAt: '2020-2-25 13:37:58',
+          endAt: '2020-2-25 13:37:58',
+          contestType: 'Accepted',
+          rule: '1',
+          creator: 'aaa'
+        }]
+    },
+    getDetail (data) {
+      return data.replace(/\n/g, '<br/>')
     }
   }
 }
@@ -49,6 +83,11 @@ export default {
     font-size: 20px;
     display: inline-block;
     text-align: left;
+  }
+  .contestRules
+  {
+    position: relative;
+    margin: 20% auto 10% auto;
   }
   .detailContentStyle
   {
