89a7be188d4a051e4382f09262bb7ab8fbe5424c
Wenke_Huang <591852399@qq.com>
2020-04-16 17:58:46 +0800

    4.16 update

--- whu_oliverjudger_FE/src/pages/stu/components/Student-Center/Profile.vue
+++ whu_oliverjudger_FE/src/pages/stu/components/Student-Center/Profile.vue
@@ -9,26 +9,12 @@
           v-model="input_name"
           clearable>
         </el-input></el-col>
-        <el-col :span="12" >Blog<el-input
-          placeholder="请输入Blog"
-          v-model="input_blog"
-          clearable>
-        </el-input></el-col>
-      </el-row>
-
-      <el-row gutter=24>
-
         <el-col :span="12" >Major<el-input
           placeholder="请输入Major"
           v-model="input_major"
           clearable>
         </el-input></el-col>
-        <el-col :span="12" >Github<el-input
-          placeholder="请输入Github"
-          v-model="input_github"
-          clearable>
-        </el-input></el-col>
-      </el-row>
+       </el-row>
       <el-button type="primary" plain>Update</el-button>
     </el-main>
   </el-container>
@@ -41,10 +27,7 @@ export default {
   data () {
     return {
       input_name: '',
-      input_blog: '',
-      input_major: '',
-      input_github: ''
-
+      input_major: ''
     }
   }
 }
--- whu_oliverjudger_Server/routes/users.js
+++ whu_oliverjudger_Server/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let users = require('../service/users')
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
