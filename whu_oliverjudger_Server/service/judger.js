1561be6d1434a2197f726ad194832cb71e090256
Wenke_Huang <591852399@qq.com>
2020-04-26 10:33:36 +0800

    4.26 ^-^

---
+++ whu_oliverjudger_Server/service/judger.js
@@ -0,0 +1,14 @@
+var exec = require('child_process').exec;
+function execute(cmd){
+
+    exec(cmd, function(error, stdout, stderr) {
+        if(error){
+            console.error(error);
+        }
+        else{
+            console.log("success");
+        }
+    });
+
+}
+execute('python3 ../EPIJudge-master/epi_judge_python_solutions/a_b_sqrt2.py');
